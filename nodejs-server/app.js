b14009b9b2574e63615afaaa23b54c72d15bfec1
Sarah O. <sarah.shofani@proton.me>
2025-10-22 17:14:57 -0300

    Redirecting Requests

--- nodejs-server/app.js
+++ nodejs-server/app.js
@@ -1,7 +1,11 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   const url = req.url;
+  const method = req.method;
+  console.log("url: ", url, "method: ", method);
+
   if (url === "/") {
     res.write("<html>");
     res.write("<head><title>Enter Message</title></head>");
@@ -9,6 +13,13 @@ const server = http.createServer((req, res) => {
       "<body><form action='/message' method='POST'><input type='text' name='message' /><button type='submit'>Send</button></form></body>",
     );
     res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/message" && method === "POST") {
+    fs.writeFileSync("message.txt", "DUMMY");
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
     return res.end();
   }
 
