234060ec99044eb68df27b48de970a7f71de8bc7
Sarah O. <sarah.shofani@proton.me>
2025-10-24 15:20:53 -0300

    Using Express Router

--- expressJs/app.js
+++ expressJs/app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const adminRoutes = require("./routes/admin.js");
+const shopRoutes = require("./routes/shop.js");
+
 const app = express();
 
 // app.use("/", (req, res, next) => {
@@ -10,19 +13,8 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    "<form action='product' method='POST'><input type='text' name='title' /><button type='submit'>Add Product</button></form>",
-  );
-});
+app.use(adminRoutes);
 
-app.post("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  res.send("<h1>Hello From Express.js</h1>");
-});
+app.use(shopRoutes);
 
 app.listen(3000);
