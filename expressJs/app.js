651692590234c41f4a5ae9789784cfaf5dafaba2
Sarah O. <sarah.shofani@proton.me>
2025-10-24 15:40:50 -0300

    Adding a 404 Error Page

--- expressJs/app.js
+++ expressJs/app.js
@@ -14,7 +14,11 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(adminRoutes);
-
 app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  console.log("Not found");
+  res.status(404).send("<h1>Page not found 404.</h1>");
+});
 
 app.listen(3000);
